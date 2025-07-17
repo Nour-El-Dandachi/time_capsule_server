f3a017f21e375dc4ebdac9c1b2c2f55923b7e3b8
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-07-17 23:48:19 +0300

    Added index to capsules table

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\User\CapsuleController;
+
 // Route::get('/capsules', [CapsuleController::class, "getAllCapsules"] );
+
+Route::group(["prefix" => "v0.1"], function(){
+    Route::group(["prefix" => "user"], function(){
+        Route::get('/capsules/{id?}', [CapsuleController::class, "getAllCapsules"] );
+        Route::get('/revealed_capsules/{id?}', [CapsuleController::class, "getRevealedCapsules"] );
+        Route::get('/closed_capsules/{id?}', [CapsuleController::class, "getClosedCapsules"] );
+        Route::post('/add_update_capsule/{id?}', [CapsuleController::class, "addOrUpdateTask"] );
+    });
+});
