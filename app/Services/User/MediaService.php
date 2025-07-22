8d2eef9945d368b25fecf1594f6c7783999f49a9
Nour El Dandachi <152388195+nourdandachi@users.noreply.github.com>
2025-07-22 04:17:20 +0300

    Handled media files

--- app/Services/User/MediaService.php
+++ app/Services/User/MediaService.php
@@ -5,6 +5,8 @@ namespace App\Services\User;
 use App\Models\Media;
 
 use App\Models\Capsule;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MediaService
 {
@@ -23,12 +25,32 @@ class MediaService
 
         return $media;
     }
-    static function createOrUpdateMedia($data, $media)
-    {
-        $media->capsule_id = 0;
-        $media->type = $data["type"] ?? $media->type;
-        $media->file_path = $data["file_path"] ?? $media->file_path;
-        
+
+    static function createOrUpdateMedia($data, $media){
+        if (isset($data['base64']) && isset($data['file_name']) && isset($data['type'])) {
+            $base64String = $data['base64'];
+
+            
+            if (Str::contains($base64String, ';base64,')) {
+                [$meta, $base64String] = explode(';base64,', $base64String);
+            }
+
+            $decoded = base64_decode($base64String);
+
+            $filename = uniqid() . '_' . preg_replace('/\s+/', '_', $data['file_name']);
+            $folder = $data['type'] === 'image' ? 'capsule_images' : 'capsule_audios';
+            $fullPath = $folder . '/' . $filename;
+
+            Storage::disk('public')->put($fullPath, $decoded);
+
+            
+
+            $media->file_path = $fullPath;
+        }
+
+        $media->capsule_id = $data['capsule_id'] ?? $media->capsule_id;
+        $media->type = $data['type'] ?? $media->type;
+
         $media->save();
         return $media;
     }
